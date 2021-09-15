582dc1e41b1045526fb160084b47be8d6fb26579
Antoine <antoinepoujaud@gmail.com>
2021-09-15 14:58:39 +0200

    update db

--- src/All_Lots/All_Lots.js
+++ src/All_Lots/All_Lots.js
@@ -1,8 +1,24 @@
+import { db } from "../Database/database"
+
 export function All_Lots(props) {
-    
+    // console.log(Object.keys(db.auctions));
+    console.log(db.lots);
+    let allLots = [];
+    for (let lot in db.lots) {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+
     return (
         <div>
-
+            
+                {/* Object.keys(db).map(key => {
+                    return <li value={key}>{db[key]}test</li>
+                }) */}
+            
         </div>
     )
 }
--- src/Database/database.js
+++ src/Database/database.js
@@ -1,32 +1,94 @@
-let db = {
+export let db = {
     auctions: {
-        arts : {
-            id: 1,
-            estimate: "1000 - 1300",
-            initialOffering: 300,
-            description: "blablablablablablabla",
-            photoURL: "http://placekitten.com/g/200/200"
+        blabla: {
+            arts: {
+                id: 1,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            }, 
+            videoGames : {
+                id: 2,
+                estimate: "300 - 500",
+                initialOffering: 50,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/200/200"
+            }, 
+            sport : {
+                id: 3,
+                estimate: "2000 - 2500",
+                initialOffering: 640,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
+            movie : {
+                id: 4,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
         },
-        videoGames : {
-            id: 2,
-            estimate: "300 - 500",
-            initialOffering: 50,
-            description: "blablablablablablabla",
-            photoURL: "http://placekitten.com/200/200"
-        }, 
-        sport : {
-            id: 3,
-            estimate: "2000 - 2500",
-            initialOffering: 640,
-            description: "blablablablablablabla",
-            photoURL: "http://placekitten.com/g/200/200"
+        test: {
+            r: {
+                id: 1,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            }, 
+            a : {
+                id: 2,
+                estimate: "300 - 500",
+                initialOffering: 50,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/200/200"
+            }, 
+            z : {
+                id: 3,
+                estimate: "2000 - 2500",
+                initialOffering: 640,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
+            g : {
+                id: 4,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
         },
-        arts : {
-            id: 4,
-            estimate: "1000 - 1300",
-            initialOffering: 300,
-            description: "blablablablablablabla",
-            photoURL: "http://placekitten.com/g/200/200"
-        }
+        aaaaaa: {
+            dsqf: {
+                id: 1,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            }, 
+            fqddff : {
+                id: 2,
+                estimate: "300 - 500",
+                initialOffering: 50,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/200/200"
+            }, 
+            udfhiu : {
+                id: 3,
+                estimate: "2000 - 2500",
+                initialOffering: 640,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
+            sdfsdf : {
+                id: 4,
+                estimate: "1000 - 1300",
+                initialOffering: 300,
+                description: "blablablablablablabla",
+                photoURL: "http://placekitten.com/g/200/200"
+            },
+        },
     },
 };
